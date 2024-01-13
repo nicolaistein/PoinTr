d1923e58f3fe6ad12053aa4c8400481af2826563
Nicolai Stein <nicolai_stein@outlook.de>
2024-01-13 18:28:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,19 +3,19 @@ HOME=`pwd`
 
 # Chamfer Distance
 cd $HOME/extensions/chamfer_dist
-python setup.py install --user
+python setup.py install
 
 # NOTE: For GRNet 
 
 # Cubic Feature Sampling
 cd $HOME/extensions/cubic_feature_sampling
-python setup.py install --user
+python setup.py install
 
 # Gridding & Gridding Reverse
 cd $HOME/extensions/gridding
-python setup.py install --user
+python setup.py install
 
 # Gridding Loss
 cd $HOME/extensions/gridding_loss
-python setup.py install --user
+python setup.py install
 
